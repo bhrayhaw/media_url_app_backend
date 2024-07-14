de69cde91cdee5d23796d94d815dd8f2376d96aa
Elijah Apreko <elijahapreko@gmail.com>
2024-07-14 18:11:54 +0000

    add client url

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}));
 app.use('/api', uploadRoutes);
 
 connectDB();
