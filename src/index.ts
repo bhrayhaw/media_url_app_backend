352411f8d9142f02fc1a86decb9a168263479045
Elijah Apreko <elijahapreko@gmail.com>
2024-07-14 18:13:27 +0000

    add headers

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,13 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: process.env.FRONTEND_URL
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use('/api', uploadRoutes);
 
 connectDB();
