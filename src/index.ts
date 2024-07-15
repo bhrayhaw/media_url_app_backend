80976c896961e859ae063b7c471f79c643b21646
Elijah Apreko <elijahapreko@gmail.com>
2024-07-15 04:56:44 +0000

    update cors options

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import uploadRoutes from './routes/uploadRoutes';
-import connectDB from './config/db';
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import uploadRoutes from "./routes/uploadRoutes";
+import connectDB from "./config/db";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
-app.use('/api', uploadRoutes);
-
 connectDB();
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use("/api", uploadRoutes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
