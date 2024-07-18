afded5f690a5614919ce1d77b1374e4f5e3eb1cf
Elijah Apreko <elijahapreko@gmail.com>
2024-07-18 12:41:15 +0000

    feat: Add fetchUrls route to retrieve URLs from the database

--- src/routes/fetchFiles.ts
+++ src/routes/fetchFiles.ts
@@ -1,1 +1,16 @@
-//Fetch files 
+import express from 'express';
+import Url from '../models/urlModel';
+
+const router = express.Router();
+
+//fetch Urls from db
+router.get('/urls', async (req, res) => {
+  try {
+    const urls = await Url.find();
+    res.json(urls);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+module.exports = router;
